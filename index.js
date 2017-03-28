c36a6baeb5bbb9bf63aa81fc4058f9ce2eb1d1fd
Jason Reeder <info@jasonreeder.com>
2017-03-28 10:52:39 -0700

    Add projection

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-d3.select('div.sky-container')
-  .append('div')
-  .html('Mr. Watson, come here. I want to see you.')
-  .style('color', 'SkyBlue')
+const width = 1000
+const height = 500
+
+const svg = d3.select('div.sky-container')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+const projection = d3.geo.equirectangular()
+  .scale(120)
+
+const path = d3.geo.path()
+  .projection(projection)
+
+const graticule = d3.geo.graticule()
+
+svg.append('path')
+  .datum(graticule)
+  .attr('d', path)
+  .attr('class', 'graticule')
